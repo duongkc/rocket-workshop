c144edefb3b11147c8d7cbed5a8e986797cb772e
Imke95 <imke_hakkens@hotmail.com>
2021-09-28 13:40:42 +0200

    total price v0

--- js/builder.js
+++ js/builder.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     class PartSwitcher {
-        constructor(parts, i) {
+        constructor(parts, prices, i) {
             i = 0;
 
             this.Next = function () {
@@ -9,7 +9,6 @@ $(document).ready(function() {
                 i++;
                 console.log(i)
                 showNextPart();
-                updatePrice();
             };
 
             this.Previous = function () {
@@ -17,7 +16,6 @@ $(document).ready(function() {
                 i--;
                 console.log(i)
                 showPreviousPart();
-                updatePrice();
             };
 
             function hideCurrentPart() {
@@ -29,10 +27,17 @@ $(document).ready(function() {
 
             function showNextPart() {
                 if (parts) {
+                    var oldIdx = i - 2;
+                    var newIdx = i - 1;
                     if (i == (parts.length)) {
                         i = 0;
                     }
                     parts[i].style.visibility = "visible";
+                    
+                    if (i == 1){
+                        oldIdx = parts.length - 1;
+                    }
+                    updatePrice(oldIdx, newIdx);
                 }
             }
 
@@ -42,21 +47,31 @@ $(document).ready(function() {
                         i = parts.length - 1;
                     }
                     parts[i].style.visibility = "visible";
+                    
+                    var newIdx = i - 1;
+                    if (i == 0){
+                        newIdx = parts.length - 1;
+                    }
+                    updatePrice(i, newIdx);
                 }
             }
 
-            function updatePrice(){
-                var newPrice = parseFloat(document.getElementById("totalPriceValue").innerHTML) + 10;
+
+            function updatePrice(oldIdx, newIdx){
+                var newPrice = parseFloat(document.getElementById("totalPriceValue").innerHTML) - prices[oldIdx] + prices[newIdx];
                 document.getElementById("totalPriceValue").innerHTML = newPrice;
             }
         }
     }
 
+    var prices_top = [100000, 250000, 50000];
+    var prices_middle = [500000, 750000, 350000];
+    var prices_bottom = [400000, 500000, 250000];
     var top = $(".bit-top");
     var middle = $(".bit-middle");
     var bottom = $(".bit-bottom");
 
-    var topPicker = new PartSwitcher(top);
+    var topPicker = new PartSwitcher(top, prices_top);
     $('#prev_top_button').click(function() {
         topPicker.Previous();
     });
@@ -64,7 +79,7 @@ $(document).ready(function() {
         topPicker.Next();
     });
 
-    var middlePicker = new PartSwitcher(middle);
+    var middlePicker = new PartSwitcher(middle, prices_middle);
     $('#prev_middle_button').click(function() {
         middlePicker.Previous();
     });
@@ -72,7 +87,7 @@ $(document).ready(function() {
         middlePicker.Next();
     });
 
-    var bottomPicker = new PartSwitcher(bottom);
+    var bottomPicker = new PartSwitcher(bottom, prices_bottom);
     $('#prev_bottom_button').click(function() {
         bottomPicker.Previous();
     });
