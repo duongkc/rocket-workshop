548ac411c04beb8649d07afcfd74a8ca86f9cc91
KC <kimchauduong@gmail.com>
2021-09-28 11:44:21 +0200

    Implement rocket part switcher
    No styling yet.

---
+++ js/builder.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+    class PartSwitcher {
+        constructor(parts, i) {
+            i = 0;
+
+            this.Next = function () {
+                hideCurrentPart();
+                i++;
+                console.log(i)
+                showNextPart();
+            };
+
+            this.Previous = function () {
+                hideCurrentPart();
+                i--;
+                console.log(i)
+                showPreviousPart();
+            };
+
+            function hideCurrentPart() {
+                if (parts) {
+                    parts[i].style.visibility = "hidden";
+
+                }
+            }
+
+            function showNextPart() {
+                if (parts) {
+                    if (i == (parts.length)) {
+                        i = 0;
+                    }
+                    parts[i].style.visibility = "visible";
+                }
+            }
+
+            function showPreviousPart() {
+                if (parts) {
+                    if (i < 0) {
+                        i = parts.length - 1;
+                    }
+                    parts[i].style.visibility = "visible";
+                }
+            }
+        }
+    }
+
+    var top = $(".bit-top");
+    var middle = $(".bit-middle");
+    var bottom = $(".bit-bottom");
+
+    var topPicker = new PartSwitcher(top);
+    $('#prev_top_button').click(function() {
+        topPicker.Previous();
+    });
+    $('#next_top_button').click(function() {
+        topPicker.Next();
+    });
+
+    var middlePicker = new PartSwitcher(middle);
+    $('#prev_middle_button').click(function() {
+        middlePicker.Previous();
+    });
+    $('#next_middle_button').click(function() {
+        middlePicker.Next();
+    });
+
+    var bottomPicker = new PartSwitcher(bottom);
+    $('#prev_bottom_button').click(function() {
+        bottomPicker.Previous();
+    });
+    $('#next_bottom_button').click(function() {
+        bottomPicker.Next();
+    });
+});
+
