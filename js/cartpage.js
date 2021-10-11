0a0260686da77ffc30c5fd7b64260573bfa9f50b
Imke95 <imke_hakkens@hotmail.com>
2021-10-11 17:15:43 +0200

    Added button functionalities on checkout page

--- js/cartpage.js
+++ js/cartpage.js
@@ -1,37 +1,8 @@
-
 class Storage{
     static getCartItems(){
         let cartItems = JSON.parse(localStorage.getItem('cart'));
         return cartItems;
     }
-}
-
-function setCartValues(cart) {
-    let tempTotal = 0;
-    let itemsTotal = 0;
-    for ( let i = 0; i < cart.length; i++){
-        tempTotal += cart[i].total;
-        itemsTotal += cart[i].amount;
-    }
-    document.getElementById("cart-total").innerHTML = formatToCurrency(tempTotal);
-    document.getElementById("cart-amount").innerHTML = itemsTotal;
-};
-
-function addCartItem(item){
-    const div = document.getElementById("cart-content-id");
-    div.innerHTML += `
-    <img class="cart-img" src="./img/rocket.jfif" alt="product"/>
-    <div>
-    <h4> Rocket </h4>
-    <h5> ${formatToCurrency(item.total)} </h5>
-    <span class = "remove-item"> remove </span>
-    </div>
-    <div>
-        <i class="fas fa-chevron-up"></i>
-        <p class="item-amount"> ${item.amount} </p>
-        <i class="fas fa-chevron-down"> </i>
-    </div>
-    `
 }
 
 function createCart(){
@@ -48,7 +19,7 @@ function setCartValues(cart) {
     let tempTotal = 0;
     let itemsTotal = 0;
     for ( let i = 0; i < cart.length; i++){
-        tempTotal += cart[i].total;
+        tempTotal += cart[i].total * cart[i].amount;
         itemsTotal += cart[i].amount;
     }
     document.getElementById("cart-total").innerHTML = formatToCurrency(tempTotal);
@@ -58,18 +29,79 @@ function setCartValues(cart) {
 
 function addCartItem(item){
     const div = document.getElementById("cart-content-id");
-    div.innerHTML += `
-    <img class="cart-img" src="./img/rocket.jfif" alt="product"/>
+    div.innerHTML += `<div class="cart-item">
+    <img class="cart-img" src="./img/rocketthumbnail.png" alt="product"/>
     <div>
     <h4> Rocket </h4>
     <h5> ${formatToCurrency(item.total)} </h5>
     <h6> ${item.top.name}, ${item.middle.name} and ${item.bottom.name}.</h6>
-    <span class = "remove-item"> remove </span>
+    <span class = "remove-item" onclick="removeItem(${item.id})"> remove </span>
     </div>
     <div>
-        <i class="fas fa-chevron-up"></i>
+        <i class="fas fa-chevron-up cart-up-icon" onclick="increaseItem(${item.id})"></i>
         <p class="item-amount"> ${item.amount} </p>
-        <i class="fas fa-chevron-down"> </i>
+        <i class="fas fa-chevron-down cart-down-icon" onclick="decreaseItem(${item.id})"> </i>
+    </div>
     </div>
     `
+}
+
+function saveCart(cart){
+    localStorage.cart = JSON.stringify(cart);
+}
+
+function clearCart(){
+    cart = [];
+    setCartValues(cart);
+    document.getElementById("cart-content-id").innerHTML = "";
+    saveCart(cart);
+}
+
+function increaseItem(id){
+    cart = Storage.getCartItems();
+    let idx = findIdx(cart, id);
+    item = cart[idx];
+    item.amount += 1;
+    document.getElementById("cart-content-id").innerHTML = "";
+    saveCart(cart);
+    createCart();
+}
+
+function decreaseItem(id){
+    cart = Storage.getCartItems();
+    let idx = findIdx(cart, id);
+    item = cart[idx];
+    item.amount -= 1;
+    if(item.amount > 0){
+        document.getElementById("cart-content-id").innerHTML = "";
+        saveCart(cart);
+        createCart();
+    } else {
+        removeItem(id);
+    }
+    
+}
+
+function removeItem(id){
+    cart = Storage.getCartItems();
+    let idx = findIdx(cart, id);
+    cart.splice(idx, 1);
+    document.getElementById("cart-content-id").innerHTML = "";
+    saveCart(cart);
+    createCart();
+}
+
+function findIdx(cart, id){
+    let idx = -1;
+    for( let i = 0; i < cart.length; i++){
+        if (cart[i].id == id){
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+function proceedCheckout(){
+    alert("You have succesfully placed an order. Your rockets will arrive shortly.");
+    clearCart();
 }
