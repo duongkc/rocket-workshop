d3f8e8d84dab87f437cafbadd69c3a033d07d2f8
KC <kimchauduong@gmail.com>
2021-10-11 09:06:54 +0200

    Add cart logic to localstorage cart

--- js/shop.js
+++ js/shop.js
@@ -2,26 +2,9 @@ var totalPrice = 0;
 var topIndex = 0;
 var middleIndex = 0;
 var bottomIndex = 0;
-let cart = [];
 
-function order(){
-    // var price = document.getElementById("totalPriceValue").innerHTML;
 
-    let rocket = {};
-    rocket.name = "Rocket";
-    rocket.top = json.rocket.top[topIndex];
-    rocket.middle = json.rocket.middle[middleIndex];
-    rocket.bottom = json.rocket.bottom[bottomIndex];
-    rocket.amount = 1; //Change later on with add/lower amount functions
-    rocket.total = totalPrice;
 
-    cart = [...cart, rocket]
-    //localStorage.cart = JSON.stringify(rocket);
-    localStorage.cart = JSON.stringify(cart);
-    console.log(JSON.parse(localStorage.cart));
-
-    console.log("You have ordered the rocket at a price of "+ formatToCurrency(totalPrice) +"!");
-}
 
 function formatToCurrency(amount){
     var formatter = new Intl.NumberFormat('en', {
