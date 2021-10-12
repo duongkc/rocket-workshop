8c5e4836d611bee7822b17a27307c1be599248a5
KC <kimchauduong@gmail.com>
2021-10-12 19:16:05 +0200

    Merge if statements
    Cleaned up code, i.e. remove unneeded comments and empty lines.
    shop.js variables are now in cart.js

--- js/shop.js
+++ js/shop.js
@@ -1,9 +1,3 @@
-var totalPrice = 0;
-var topIndex = 0;
-var middleIndex = 0;
-var bottomIndex = 0;
-
-
 function formatToCurrency(amount){
     var formatter = new Intl.NumberFormat('en', {
         style: 'currency',
